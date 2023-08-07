f6431fd289193883b1c98406cd84e49d8cab11ed
Ade730 <adeniyipaul77@gmail.com>
2023-08-07 16:39:54 +0100

    1st commit

--- Boostrap Project/weather app/main.js
+++ Boostrap Project/weather app/main.js
@@ -1,18 +1,13 @@
 const apikey = "9dcc48a308ec20b11ae10538bff3c429";
 const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-
 const searchbox = document.querySelector(".search input");
 const searchbtn = document.querySelector(".search button");
 const weathericon = document.querySelector(".weather-icon")
-
-
 async function checkWeather(city) {
     const response = await fetch(apiurl + city + `&appid=${apikey}`);
     var data = await response.json()
 
     console.log(data);
-
-
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
